3675112ac920e11c89a63a1dcc9754e3d3778c40
Sebastian Zimmer <Sebastian.Zimmer@softwire.com>
2016-07-11 17:22:32 +0100

    Make use of beforeEach to shorten tests.

--- __tests__/queues-test.ts
+++ __tests__/queues-test.ts
@@ -6,16 +6,16 @@ jest.unmock("../src/Queues");
 import { Queue } from "../src/Queue";
 import { Queues } from "../src/Queues";
 
-function getExampleQueues(): Queues {
-    return new Queues([
+describe("Queues", () => {
+    let queues: Queues;
+    beforeEach(() => {
+        queues = new Queues([
         new Queue("First Queue", 17, ["Bob"]),
         new Queue("Second Queue", 11, ["John", "Bob"]),
         new Queue("Third Queue", 41, ["John"])]);
-}
+    });
 
-describe("Queues", () => {
     it("can find the index of a queue", () => {
-        let queues = getExampleQueues();
         expect(queues.indexOf(17)).toBe(0);
         expect(queues.indexOf(11)).toBe(1);
         expect(queues.indexOf(41)).toBe(2);
@@ -23,14 +23,12 @@ describe("Queues", () => {
     });
 
     it("can delete a queue", () => {
-        let queues = getExampleQueues();
         queues.deleteQueue(11);
         expect(queues.queues.length).toBe(2);
         expect(queues.queues[1].id).toBe(41);
     });
 
     it("can push a queue", () => {
-        let queues = getExampleQueues();
         let newQueue = new Queue("Fourth Queue", 15, []);
         queues.pushQueue(newQueue);
         expect(queues.queues.length).toBe(4);
@@ -38,13 +36,11 @@ describe("Queues", () => {
     });
 
     it("can make a user join a queue", () => {
-        let queues = getExampleQueues();
         queues.toggleJoin(41, "Bob");
         expect(queues.queues[2].users).toEqual(["John", "Bob"]);
     });
 
     it("can make a user leave a queue", () => {
-        let queues = getExampleQueues();
         queues.toggleJoin(11, "Bob");
         expect(queues.queues[2].users).toEqual(["John"]);
     });
